71c54f3cb465994c4fda28d00c5b91bbcf191732
mj <the.moj.moein@gmail.com>
2025-05-17 23:23:28 +0330

    add test for troops uniqueness

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ composer install
 ```bash
 php index.php
 ```
+
+## Running Tests
+
+This project uses PHPUnit for testing. To run the tests, follow these steps:
+
+1. Make sure you have installed the dependencies, including PHPUnit:
+
+```bash
+vendor/bin/phpunit
+```
--- tests/TroopsTest.php
+++ tests/TroopsTest.php
@@ -11,4 +11,18 @@ class TroopsTest extends \PHPUnit\Framework\TestCase
         $this->assertGreaterThan(0, $archers);
         $this->assertEquals($input, ($spearmen+$swordsmen+$archers));
     }
+
+    public function test_troops_uniqueness()
+    {
+        $input = rand(3, 1000);
+        list($spearmen1, $swordsmen1, $archers1) = \Stillfront\Service\TroopsFactory::generateTroops($input);
+        list($spearmen2, $swordsmen2, $archers2) = \Stillfront\Service\TroopsFactory::generateTroops($input);
+        $allEqual = (
+            $spearmen1 === $spearmen2 &&
+            $swordsmen1 === $swordsmen2 &&
+            $archers1 === $archers2
+        );
+        $this->assertFalse($allEqual);
+
+    }
 }
